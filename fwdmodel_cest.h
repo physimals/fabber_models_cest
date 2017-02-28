43ae1459ec879a42266bafb689b0bc4a58e81dda
Alex Smith <alex.smith@ndcn.ox.ac.uk>
2017-02-28 13:48:55 +0000

    Initial Commit for New Branch. fwdmodel_cest.h -Added language in header file to define new pulse specifications. fwdmodel_cest.cc -Updated wlam (Initialize) so that it reflects proper conversion between ppm and Hz

--- fwdmodel_cest.h
+++ fwdmodel_cest.h
@@ -1,8 +1,8 @@
 /*  fwdmodel_cest_devel.h - Development resting state ASL model
 
-    Michael Chappell, IBME PUMMA & FMRIB Image Analysis Group
+ Michael Chappell, IBME PUMMA & FMRIB Image Analysis Group
 
-    Copyright (C) 2010 University of Oxford  */
+ Copyright (C) 2010 University of Oxford  */
 
 /*  CCOPYRIGHT */
 
@@ -12,106 +12,137 @@
 #include <string>
 using namespace std;
 
-class CESTFwdModel : public FwdModel {
+class CESTFwdModel: public FwdModel
+{
 public:
-  static FwdModel* NewInstance();
-  
-  // Virtual function overrides
-  virtual void Initialize(ArgsType& args);
-  virtual void Evaluate(const NEWMAT::ColumnVector& params, 
-			      NEWMAT::ColumnVector& result) const;
-   virtual vector<string> GetUsage() const;
-   virtual string ModelVersion() const;
-   virtual void GetOptions(std::vector<OptionSpec> &opts) const;
-   virtual std::string GetDescription() const;
-               
-   virtual void DumpParameters(const NEWMAT::ColumnVector& vec,
-                                const string& indents = "") const;
-                                
-   virtual void NameParams(vector<string>& names) const;     
-   virtual int NumParams() const 
-   { return (3*npool - 1) + 1 + ( inferdrift?1:0 ) + ( t12soft? (2*npool):0 ) + (3*nexpool);
-   } 
-   
-   virtual ~CESTFwdModel() { return; }
-   
-   virtual void HardcodedInitialDists(MVNDist& prior, MVNDist& posterior) const;
-   virtual void InitParams(MVNDist& posterior) const;
-   
+	static FwdModel* NewInstance();
 
-protected: 
-  //specific functions
-  void Mz_spectrum(NEWMAT::ColumnVector& Mz, const NEWMAT::ColumnVector& wvec, const NEWMAT::ColumnVector& w1, const NEWMAT::ColumnVector& t, const NEWMAT::ColumnVector& M0, const  NEWMAT::Matrix& wi, const  NEWMAT::Matrix& kij, const  NEWMAT::Matrix& T12) const;
-   NEWMAT::ReturnMatrix Mz_spectrum_lorentz(const NEWMAT::ColumnVector& wvec, const NEWMAT::ColumnVector& w1, const NEWMAT::ColumnVector& t, const NEWMAT::ColumnVector& M0, const  NEWMAT::Matrix& wi, const  NEWMAT::Matrix& kij, const  NEWMAT::Matrix& T12) const;
+	// Virtual function overrides
+	virtual void Initialize(ArgsType& args);
+	virtual void Evaluate(const NEWMAT::ColumnVector& params,
+			NEWMAT::ColumnVector& result) const;
+	virtual vector<string> GetUsage() const;
+	virtual string ModelVersion() const;
+	virtual void GetOptions(std::vector<OptionSpec> &opts) const;
+	virtual std::string GetDescription() const;
 
-  void Mz_contribution_lorentz_simple(NEWMAT::ColumnVector& Mzc, const NEWMAT::ColumnVector& wvec, const double& I, const NEWMAT::ColumnVector& wi, const double& R) const;
+	virtual void DumpParameters(const NEWMAT::ColumnVector& vec,
+			const string& indents = "") const;
 
-  //maths functions
-  void Ainverse(const  NEWMAT::Matrix A,  NEWMAT::RowVector& Ai) const;
-   NEWMAT::ReturnMatrix expm( NEWMAT::Matrix inmatrix) const;
-   NEWMAT::ReturnMatrix expm_eig( NEWMAT::Matrix inmatrix) const;
-   NEWMAT::ReturnMatrix expm_pade( NEWMAT::Matrix inmatrix) const;
-   NEWMAT::ReturnMatrix PadeApproximant( NEWMAT::Matrix inmatrix, int m) const;
-   NEWMAT::ReturnMatrix PadeCoeffs(int m) const;
+	virtual void NameParams(vector<string>& names) const;
+	virtual int NumParams() const
+	{
+		return (3 * npool - 1) + 1 + (inferdrift ? 1 : 0)
+				+ (t12soft ? (2 * npool) : 0) + (3 * nexpool);
+	}
+
+	virtual ~CESTFwdModel()
+	{
+		return;
+	}
+
+	virtual void HardcodedInitialDists(MVNDist& prior,
+			MVNDist& posterior) const;
+	virtual void InitParams(MVNDist& posterior) const;
+
+protected:
+	//specific functions
+	void Mz_spectrum(NEWMAT::ColumnVector& Mz, const NEWMAT::ColumnVector& wvec,
+			const NEWMAT::ColumnVector& w1, const NEWMAT::ColumnVector& t,
+			const NEWMAT::ColumnVector& M0, const NEWMAT::Matrix& wi,
+			const NEWMAT::Matrix& kij, const NEWMAT::Matrix& T12) const;
+	NEWMAT::ReturnMatrix Mz_spectrum_lorentz(const NEWMAT::ColumnVector& wvec,
+			const NEWMAT::ColumnVector& w1, const NEWMAT::ColumnVector& t,
+			const NEWMAT::ColumnVector& M0, const NEWMAT::Matrix& wi,
+			const NEWMAT::Matrix& kij, const NEWMAT::Matrix& T12) const;
+
+	void Mz_contribution_lorentz_simple(NEWMAT::ColumnVector& Mzc,
+			const NEWMAT::ColumnVector& wvec, const double& I,
+			const NEWMAT::ColumnVector& wi, const double& R) const;
+
+	// Steady State Mz Spectrum
+	void Mz_spectrum_SS(NEWMAT::ColumnVector& Mz,
+			const NEWMAT::ColumnVector& wvec, const NEWMAT::ColumnVector& w1,
+			const NEWMAT::ColumnVector& t, const NEWMAT::ColumnVector& M0,
+			const NEWMAT::Matrix& wi, const NEWMAT::Matrix& kij,
+			const NEWMAT::Matrix& T12, const NEWMAT::ColumnVector& w1EX) const;
+
+	//maths functions
+	void Ainverse(const NEWMAT::Matrix A, NEWMAT::RowVector& Ai) const;
+	NEWMAT::ReturnMatrix expm(NEWMAT::Matrix inmatrix) const;
+	NEWMAT::ReturnMatrix expm_eig(NEWMAT::Matrix inmatrix) const;
+	NEWMAT::ReturnMatrix expm_pade(NEWMAT::Matrix inmatrix) const;
+	NEWMAT::ReturnMatrix PadeApproximant(NEWMAT::Matrix inmatrix, int m) const;
+	NEWMAT::ReturnMatrix PadeCoeffs(int m) const;
 
 // Constants
 
-  // Lookup the starting indices of the parameters
-  
+// Lookup the starting indices of the parameters
 
-  // vector indices for the parameters to expereicne ARD
-  vector<int> ard_index;
+	// vector indices for the parameters to expereicne ARD
+	vector<int> ard_index;
 
-  //flags
-  bool t12soft;
-  bool inferdrift;
-  //bool pvcorr;
-  bool lorentz;
-  bool steadystate;
-  bool setconcprior;
+	//flags
+	bool t12soft;
+	bool inferdrift;
+	//bool pvcorr;
+	bool lorentz;
+	bool steadystate;
+	bool setconcprior;
 
-  // scan parameters
-  vector<float> t;
+	// scan parameters
+	vector<float> t;
 
-  //model parameters
-  int npool;
-  float wlam;
-  float B1set;
-  NEWMAT::ColumnVector poolppm;
-  NEWMAT::ColumnVector poolk;
-   NEWMAT::Matrix T12master;
-  NEWMAT::ColumnVector poolcon;
-  NEWMAT::ColumnVector poolconprec;
-  // NEWMAT::Matrix T12WMmaster;
-  // NEWMAT::Matrix T12CSFmaster;
+	//model parameters
+	int npool;
+	float wlam;
+	float B1set;
+	NEWMAT::ColumnVector poolppm;
+	NEWMAT::ColumnVector poolk;
+	NEWMAT::Matrix T12master;
+	NEWMAT::ColumnVector poolcon;
+	NEWMAT::ColumnVector poolconprec;
+	// NEWMAT::Matrix T12WMmaster;
+	// NEWMAT::Matrix T12CSFmaster;
 
-  // MT pool
-  //bool mtpool;
+	// MT pool
+	//bool mtpool;
 
-  // extra pools
-  int nexpool;
-  NEWMAT::ColumnVector expoolppm;
-  NEWMAT::ColumnVector expoolR;
+	// extra pools
+	int nexpool;
+	NEWMAT::ColumnVector expoolppm;
+	NEWMAT::ColumnVector expoolR;
 
-  // Data specification
-  NEWMAT::ColumnVector wvec;
-  NEWMAT::ColumnVector w1vec;
-  NEWMAT::ColumnVector tsatvec;
+	// Data specification
+	NEWMAT::ColumnVector wvec;
+	NEWMAT::ColumnVector w1vec;
+	NEWMAT::ColumnVector tsatvec;
 
-  //pulse specificaiton
-  NEWMAT::ColumnVector pmagvec;
-  NEWMAT::ColumnVector ptvec;
-  int nseg;
+	//pulse specificaiton
+	NEWMAT::ColumnVector pmagvec;
+	NEWMAT::ColumnVector ptvec;
+	int nseg;
 
-  // processing flags
-  mutable bool fastgrad; //use a fast approximation to the expm because we are caculating the gradient
-  
-  // ard flags
-  bool doard;
+	//Excitation Pulse specification
+	NEWMAT::ColumnVector m_EXmagvec;
+	NEWMAT::ColumnVector m_EXtvec;
+	NEWMAT::ColumnVector m_EXmagMax;
+	int m_nEXseg;
 
-  private:
-  /** Auto-register with forward model factory. */
-  static FactoryRegistration<FwdModelFactory, CESTFwdModel> registration;
+	//Readout specifications
+	NEWMAT::ColumnVector m_TR;
 
- 
+	// Bool for New Steady State Sequence
+	bool m_SS;
+
+	// processing flags
+	mutable bool fastgrad; //use a fast approximation to the expm because we are caculating the gradient
+
+	// ard flags
+	bool doard;
+
+private:
+	/** Auto-register with forward model factory. */
+	static FactoryRegistration<FwdModelFactory, CESTFwdModel> registration;
+
 };
